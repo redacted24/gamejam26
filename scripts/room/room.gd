adb35986958b6ca809a845de73e3fbdabc24ae3c
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-01-31 11:47:35 -0500

    wip: script for room changing

--- scripts/room/room.gd
+++ scripts/room/room.gd
@@ -4,12 +4,25 @@ class_name Room
 var enemy_count : int
 var door_count : int
 var is_cleared : bool
+var all_doors : Array # array holds all doors (exits)
 
 func _ready() -> void:
 	is_cleared = false
 	print("Loading new room")
+	# Connect spawn location signal
 	if NavManager and NavManager.spawn_location != null:
 		_on_level_spawn(NavManager.spawn_location)
+	
+	# Get all door nodes
+	all_doors = get_node("Doors").get_children()
+	door_count = all_doors.size()
+	
+	# iterate through doors and assign their teleport
+	for door : Door in all_doors:
+		var next_room_path = generate_next_rooms()
+		door.next_level_path = next_room_path
+		# all rooms should only have one spawnpoint for now
+		door.next_level_spawnpoint = "main"
 
 func spawn_enemies() -> void:
 	pass
@@ -17,14 +30,18 @@ func spawn_enemies() -> void:
 # Function to determine what type of rooms the next rooms should be, based on the number of doors or exit
 # Returns a string containing the path to the room type scene yay
 func generate_next_rooms() -> String:
+	var next_room_path : String
 	var next_room_type = MapGeneration.next_room_type()
 	if next_room_type is PeacefulRoom:
-		return "res://scenes/rooms/types/peaceful_room.tscn"
+		next_room_path = "res://scenes/rooms/types/peaceful_room.tscn"
 	elif next_room_type is CombatRoom:
-		return "res://scenes/rooms/types/combat_room.tscn"
+		next_room_path = "res://scenes/rooms/types/combat_room.tscn"
 	elif next_room_type is CrossroadsRoom:
-		return "res://scenes/rooms/types/room_crossroads.tscn"
-	return ""
+		next_room_path = "res://scenes/rooms/types/room_crossroads.tscn"
+	else:
+		next_room_path = ""
+	print("next_room path is:")
+	return next_room_path
 	
 # Function that determines where the player should spawn
 # Emits the signal for the player to spawn
