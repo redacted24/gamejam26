cbe95e0b1cbd45090146f697a88f8a2807d97927
Rayyan <rayyan.lodhi05@gmail.com>
2026-01-31 14:55:26 -0500

    simple spear done

--- global/event_bus.gd
+++ global/event_bus.gd
@@ -2,14 +2,14 @@ extends Node
 
 # Player signals
 signal player_hunger_reduced(amount : int)
-#signal player_damaged(current_hp: int, max_hp: int)
+signal player_damaged(current_hp: int, max_hp: int)
 #signal player_healed(current_hp: int, max_hp: int)
 signal player_died
 #signal player_stats_changed(stats: Dictionary)
 #signal player_entered_door(direction: Vector2i)
 
 # Enemy signals
-#signal enemy_died(pos: Vector2)
+signal enemy_died(pos: Vector2)
 #signal enemy_spawned
 
 # Room signals
