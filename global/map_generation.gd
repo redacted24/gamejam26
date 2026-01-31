3e2465b936c1911a1e8b755166e7009817811848
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-01-31 12:58:25 -0500

    fix: combat room not appearing in random checks

--- global/map_generation.gd
+++ global/map_generation.gd
@@ -1,12 +1,11 @@
 extends Node
 
-enum room_types {COMBAT_ROOM, PEACEFUL_ROOM, CROSSROADS_ROOM, RATS_ROOM}
+enum room_types {COMBAT_ROOM, PEACEFUL_ROOM, CROSSROADS_ROOM, END_ROOM}
 
 var room_spawn_limit = {
 	peaceful_room = 1,
 	combat_room = 2,
 	crossroads_room = 1,
-	rats_room = 1
 }
 
 func _ready() -> void:
@@ -14,16 +13,16 @@ func _ready() -> void:
 
 # Finds what the next room type is and link it to a "door" in a level
 func next_room_type():
+	print(room_spawn_limit)
 	var rand = randf()
-	if rand <= 0.25 and room_spawn_limit.peaceful_room > 0:
+	if rand <= 0.333 and room_spawn_limit.peaceful_room > 0:
 		return room_types.PEACEFUL_ROOM
-	elif rand <= 0.5 and room_spawn_limit.combat_room > 0:
+	if rand <= 0.66 and room_spawn_limit.combat_room > 0:
 		return room_types.COMBAT_ROOM
-	elif rand <= 0.75 and room_spawn_limit.crossroads_room > 0:
+	if rand < 1 and room_spawn_limit.crossroads_room > 0:
 		return room_types.CROSSROADS_ROOM
-	elif rand < 1 and room_spawn_limit.rats_room > 0:
-		return room_types.RATS_ROOM
-	pass
+	# No more rooms to spawn!
+	return room_types.END_ROOM
 
 # Update the room count based on what room was taken
 func update_room_count(room) -> void:
@@ -33,6 +32,4 @@ func update_room_count(room) -> void:
 		room_spawn_limit.combat_room -= 1
 	elif room == room_types.CROSSROADS_ROOM:
 		room_spawn_limit.crossroads_room -= 1
-	elif room == room_types.RATS_ROOM:
-		room_spawn_limit.rats_room -= 1
 	pass
--- global/nav_manager.gd
+++ global/nav_manager.gd
@@ -10,9 +10,8 @@ func go_to_level(level_path : String, spawn_point : String):
 	var scene_to_load := load(level_path)
 	if scene_to_load != null:
 		spawn_location = spawn_point
-		get_tree().change_scene_to_packed(scene_to_load)
+		get_tree().call_deferred("change_scene_to_packed", scene_to_load)
 		print("changed scene to %s" % scene_to_load.to_string())
 
 func trigger_player_spawn(spawn_position) -> void:
-	print("emitting player spawn signal")
 	player_spawn.emit(spawn_position)
