20e597d73b106423591bcec1c67698c3ce4b7f4a
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-01-30 20:06:34 -0500

    init state machine

---
+++ scripts/state.gd
@@ -0,0 +1,17 @@
+extends Node
+
+class_name State
+
+signal Transitioned
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func process(_delta: float) -> void:
+	pass
+	
+func physics_process(_delta: float) -> void:
+	pass
