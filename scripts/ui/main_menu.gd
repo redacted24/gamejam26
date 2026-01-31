102ab6cbf5d183f6dbadf852c0c1e2091ab61aa2
Rayyan <rayyan.lodhi05@gmail.com>
2026-01-31 01:00:25 -0500

    added simple main menu screen

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var settings_panel: Panel = $SettingsPanel
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_settings_button_pressed() -> void:
+	settings_panel.visible = true
+
+func _on_close_button_pressed() -> void:
+	settings_panel.visible = false
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	var db := linear_to_db(value / 100.0)
+	AudioServer.set_bus_volume_db(0, db)
