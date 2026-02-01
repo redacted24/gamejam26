169c8856561b12bee84e57327ec5c39e5bf0789a
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-02-01 09:18:49 -0500

    fix

--- scenes/rooms/types/cutscenes/cutscene2.gd
+++ scenes/rooms/types/cutscenes/cutscene2.gd
@@ -1,11 +1,16 @@
 extends Room
 
 @export_file_path var resource_path : String
+@onready var door_collision : CollisionShape2D = $Doors/HorizontalDoor/CollisionShape2D
 
 # Hard-coded cutscene
 
 func _ready() -> void:
+	# Hide doors because it is currently directly on the player. Only unhide collision at the end of cutscene
+	door_collision.set_deferred("disabled", true)
+	# Signals handling (emitting and connecting)
 	EventBus.cutscene_enter.emit()
+	DialogueManager.dialogue_ended.connect(_on_dialogue_end)
 	print("entered cutscene 2 state")
 	# Play fade in animation
 	SceneChanger.animation_player.play("long_fade_in")
@@ -16,6 +21,11 @@ func _ready() -> void:
 	await timer.timeout
 	var resource : DialogueResource = load(resource_path)
 	show_example_dialogue_baloon(resource, "start", [])
+	
+func _on_dialogue_end(resource : DialogueResource) -> void:
+	print("reenabling door collision")
+	door_collision.set_deferred("disabled", false)
+	
 
 func show_example_dialogue_baloon(resource: DialogueResource, title: String = "", extra_game_states: Array = []):
 	var balloon : Node = load("res://scenes/dialogue/balloon.tscn").instantiate()
