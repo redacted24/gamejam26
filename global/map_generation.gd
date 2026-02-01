4729ad3cc356c240d354a982051e044d82e254b9
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-02-01 01:26:39 -0500

    add[roomgen]: tutorial1 room

--- global/map_generation.gd
+++ global/map_generation.gd
@@ -55,46 +55,8 @@ const CROSSROADS_ROOM_PATHS = [
 	"res://scenes/rooms/types/crossroads/crossroads_2.tscn",
 ]
 
-
-var room_spawn_limit = {
-	peaceful_room = 2,
-	combat_room = 3,
-	crossroads_room = 1,
-}
-
 func _ready() -> void:
-	EventBus.connect("room_cleared", update_room_count)
 	seed(12345)
-
-# Finds what the next room type is and link it to a "door" in a level
-func get_next_room_type():
-	var rand = randf()
-	# If all room numbers are exhausted.
-	if room_spawn_limit.peaceful_room == 0 and room_spawn_limit.combat_room == 0 and room_spawn_limit.crossroads_room == 0:
-		return room_types.END_ROOM
-		
-	# Setup rolling until you get something usable
-	
-	while true:
-		rand = randf()		
-		if rand <= 0.333 and room_spawn_limit.peaceful_room > 0:
-			return room_types.PEACEFUL_ROOM
-		if rand <= 0.66 and room_spawn_limit.combat_room > 0:
-			return room_types.COMBAT_ROOM
-		if rand < 1 and room_spawn_limit.crossroads_room > 0:
-			return room_types.CROSSROADS_ROOM
-	# No more rooms to spawn!
-
-# Update the room count based on what room was taken
-func update_room_count(room : MapGeneration.room_types) -> void:
-	print("updating room type %s" % room_spawn_limit)
-	if room == room_types.PEACEFUL_ROOM:
-		room_spawn_limit.peaceful_room -= 1
-	elif room == room_types.COMBAT_ROOM:
-		room_spawn_limit.combat_room -= 1
-	elif room == room_types.CROSSROADS_ROOM:
-		room_spawn_limit.crossroads_room -= 1
-	pass
 
 # Function to determine what type of rooms the next rooms should be, based on the number of doors or exit
 # Returns an array containing: string containing the path to the room type scene, and type of next room
@@ -104,7 +66,7 @@ func generate_next_rooms() -> Dictionary:
 		"type": MapGeneration.room_types
 	}
 	
-	var next_room_type = get_next_room_type() # randomly generate next room type
+	var next_room_type : MapGeneration.room_types = stage_types[0]
 	var num_that_type : int
 	var rand_idx : int
 	
