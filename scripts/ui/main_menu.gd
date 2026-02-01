707ee0033308388c625934256724c2a91003bee5
Peizhe Guan <peizheg@gmail.com>
2026-01-31 19:03:49 -0500

    improve settings page

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@onready var settings_panel: Panel = $SettingsPanel
 @onready var join_panel: VBoxContainer = $JoinPanel
 @onready var ip_input: LineEdit = $JoinPanel/IpInput
 @onready var status_label: Label = $StatusLabel
@@ -79,11 +78,4 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_settings_button_pressed() -> void:
-	settings_panel.visible = true
-
-func _on_close_button_pressed() -> void:
-	settings_panel.visible = false
-
-func _on_volume_slider_value_changed(value: float) -> void:
-	var db := linear_to_db(value / 100.0)
-	AudioServer.set_bus_volume_db(0, db)
+	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
