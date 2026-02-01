c093719f33721bfdcbc65ee5bc783c35264b1977
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-02-01 02:32:06 -0500

    feat: done system for stages and room types

--- global/map_generation.gd
+++ global/map_generation.gd
@@ -64,6 +64,21 @@ const SHOP_ROOM_PATHS = [
 	"res://scenes/rooms/types/shop/shop.tscn"
 ]
 
+# All variations of boss rooms
+const BOSS_ROOM_PATHS = [
+	"res://scenes/rooms/types/boss/boss.tscn"
+]
+
+# All variations of end rooms
+const END_ROOM_PATHS = [
+	"res://scenes/rooms/types/end_room.tscn"
+]
+
+# All variations of fox rooms
+const FOX_ROOM_PATHS = [
+	"res://scenes/rooms/types/fox/fox.tscn"
+]
+
 func _ready() -> void:
 	seed(12345)
 
@@ -72,60 +87,45 @@ func _ready() -> void:
 func generate_next_rooms() -> Dictionary:
 	var out = {
 		"path": "",
-		"type": MapGeneration.room_types
+		"type": null
 	}
 	
-	var next_room_type : MapGeneration.room_types = stage_types[0]
-	print("next room type is %s" % next_room_type)
+	current_stage += 1
+	var next_room_type : room_types = stage_types[current_stage]
 	var num_that_type : int
 	var rand_idx : int
 	
 	# Match next room type and get the proper resource for it
-	var en = MapGeneration.room_types
-	match next_room_type:
-		en.PEACEFUL_ROOM:
-			num_that_type = PEACEFUL_ROOM_PATHS.size()
-			rand_idx = randi() & num_that_type
-			out.path = PEACEFUL_ROOM_PATHS[rand_idx]
-			out.type = en.PEACEFUL_ROOM
-		en.COMBAT_ROOM:
-			num_that_type = COMBAT_ROOM_PATHS.size()
-			rand_idx = randi() % num_that_type
-			out.path = COMBAT_ROOM_PATHS[rand_idx]
-			out.type = en.COMBAT_ROOM
-		en.TUTORIAL_ROOM:
-			out.path = TUTORIAL_ROOM_PATHS[0]
-			out.type = en.TUTORIAL_ROOM
-		en.CROSSROADS_ROOM:
-			num_that_type = CROSSROADS_ROOM_PATHS.size()
-			rand_idx = randi() & num_that_type
-			out.path = CROSSROADS_ROOM_PATHS[rand_idx]
-			out.type = en.CROSSROADS_ROOM
-		en.SHOP_ROOM:
-			pass
-		en.BOSS_ROOM:
-			pass
-		en.FOX_ROOM:
-			pass
-		en.END_ROOM:
-			pass
-	
-	if next_room_type == MapGeneration.room_types.PEACEFUL_ROOM:
+	var en = room_types
+	if next_room_type == en.PEACEFUL_ROOM:
 		num_that_type = PEACEFUL_ROOM_PATHS.size()
 		rand_idx = randi() % num_that_type
 		out.path = PEACEFUL_ROOM_PATHS[rand_idx]
-		out.type = MapGeneration.room_types.PEACEFUL_ROOM
-	elif next_room_type == MapGeneration.room_types.COMBAT_ROOM:
+		out.type = en.PEACEFUL_ROOM
+	elif next_room_type == en.COMBAT_ROOM:
 		num_that_type = COMBAT_ROOM_PATHS.size()
 		rand_idx = randi() % num_that_type
 		out.path = COMBAT_ROOM_PATHS[rand_idx]
-		out.type = MapGeneration.room_types.COMBAT_ROOM
-	elif next_room_type == MapGeneration.room_types.CROSSROADS_ROOM:
+		out.type = en.COMBAT_ROOM
+	elif next_room_type == en.TUTORIAL_ROOM:
+		out.path = TUTORIAL_ROOM_PATHS[0]
+		out.type = en.TUTORIAL_ROOM
+	elif next_room_type == en.CROSSROADS_ROOM:
 		num_that_type = CROSSROADS_ROOM_PATHS.size()
 		rand_idx = randi() % num_that_type
 		out.path = CROSSROADS_ROOM_PATHS[rand_idx]
-		out.type = MapGeneration.room_types.CROSSROADS_ROOM
-	elif next_room_type == MapGeneration.room_types.END_ROOM:
-		out.path = "res://scenes/rooms/types/end_room.tscn"
-		out.type = MapGeneration.room_types.END_ROOM
+		out.type = en.CROSSROADS_ROOM
+	elif next_room_type == en.SHOP_ROOM:
+		out.path = SHOP_ROOM_PATHS[0]
+		out.type = en.SHOP_ROOM
+	elif next_room_type == en.BOSS_ROOM:
+		out.path = BOSS_ROOM_PATHS[0]
+		out.type = en.BOSS_ROOM
+	elif next_room_type == en.FOX_ROOM:
+		out.path = FOX_ROOM_PATHS[0]
+		out.type = en.FOX_ROOM
+	elif next_room_type == en.END_ROOM:
+		out.path = END_ROOM_PATHS[0]
+		out.type = en.END_ROOM
+
 	return out
