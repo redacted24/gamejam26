1c7e300dd273fc7f541394a2b895910403ae3b6d
Rayyan <rayyan.lodhi05@gmail.com>
2026-02-01 03:24:05 -0500

    plays first dialogue on scene load

--- scripts/room/cutscene_1.gd
+++ scripts/room/cutscene_1.gd
@@ -5,10 +5,18 @@ var dialogue_triggered := false
 
 func _ready() -> void:
 	super._ready()
+	# Play intro dialogue on load
+	play_intro_dialogue()
 	# Connect the spawnpoint2 area signal
 	var spawnpoint2 = get_node_or_null("SpawnPoints/Spawnpoint2/Area2D")
 	if spawnpoint2:
 		spawnpoint2.body_entered.connect(_on_spawnpoint2_body_entered)
+
+func play_intro_dialogue() -> void:
+	var resource: DialogueResource = load("res://dialogues/1_post_monster_monologue.dialogue")
+	var balloon: Node = load("res://scenes/dialogue/balloon.tscn").instantiate()
+	get_tree().current_scene.add_child(balloon)
+	DialogueManager.show_dialogue_balloon_scene(balloon, resource, "start")
 
 func _on_spawnpoint2_body_entered(body: Node2D) -> void:
 	if dialogue_triggered:
